2e2ed952feb2cc05257f5e8af18154eb2725dc64
Victor Chandra <41635105+mzmznasipadang@users.noreply.github.com>
2024-10-25 13:20:19 +0700

    test

--- sanity/nps-website/sanity.config.ts
+++ sanity/nps-website/sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig, Plugin} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import { vercelDeployTool } from 'sanity-plugin-vercel-deploy'
 
 export default defineConfig({
   name: 'default',
@@ -12,7 +13,7 @@ export default defineConfig({
 
   plugins: [
     structureTool(),
-    ...(process.env.NODE_ENV === 'development' ? [visionTool()] : [])
+    ...(process.env.NODE_ENV === 'development' ? [visionTool()] : []),  vercelDeployTool(),
   ],
 
   schema: {
